1cae8064aba7b941d6d3e417e006836970933c2c
shifali <shifali.gupta183@gmail.com>
2019-06-08 00:34:38 +0530

    integrating giphy display

--- components/app/app.js
+++ components/app/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navigation from '../navigation/navigation.container';
 import styles from './app.css';
+import GiphyDisplay from '../giphy-display/giphy-display';
 
 export default({children})=>(   
     <div>
-    <h1 className={styles.header}>Giphy Master</h1>
-    <Navigation />
+      <h1 className={styles.header}>Giphy Master</h1>
+      <Navigation />
       {children}
     </div>
 )
---
+++ components/giphy-display/giphy-display.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import * as customTypes from '../../lib/custom-types';
+import classNames from 'classnames';
+import styles from './giphy-display.css';
+
+
+export default function GiphyDisplay({isShown, giphy, onClick}){
+    const containerClass= classNames(
+        styles.container,
+        {
+            [styles.isShown]: isShown
+        }
+    )
+    return (
+        <div className={containerClass} onClick={onClick}>
+            <img className={styles.image} src={giphy.full} />
+        </div>
+    );
+
+}
+
+GiphyDisplay.propTypes= {
+    isShown: PropTypes.bool.isRequired,
+    giphy: customTypes.searchResult.isRequired,
+    onClick: PropTypes.func.isRequired
+};
