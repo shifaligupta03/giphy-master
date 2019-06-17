187126699bcbb0af8a3905748d104c2e1dd29a8c
Rajender Joshi <me@sonujoshi.com>
2019-06-17 23:02:02 +0530

    Type fix

--- reducers/giphy-display.js
+++ reducers/giphy-display.js
@@ -6,7 +6,7 @@ const initialState={
 export default (state = initialState, action ) =>{
     switch(action.type){
         case SHOW_GIPHY:
-            return { ...state, giphygiphyDisplayed: action.giphyDisplayed, giphyDisplayIsShown: true };
+            return { ...state, giphyDisplayed: action.giphyDisplayed, giphyDisplayIsShown: true };
         case HIDE_GIPHY:
             return { ...state, giphyDisplayed: initialState.giphyDisplayed, giphyDisplayIsShown: false };
         default: 
